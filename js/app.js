7a2cd5a490a8fe109712d5df5d603d4b2dd490d7
Tim Harshman <goteamtim+git@gmail.com>
2016-05-14 16:29:47 -0700

    Woking on picture flow

--- js/app.js
+++ js/app.js
@@ -20,16 +20,48 @@ window.addEventListener("DOMContentLoaded", ()=>{
     }
     
     document.getElementById("takePicture").addEventListener("click",()=>{
-    context.drawImage(video,0,0,640,480);
+    takePictures();
     //take four pictures here?  add them up?
+    
 });
 },false);
 
 
 
 function takePictures(){
-    
+    let countdown = 3;
+    //set refreshIntervalID so you can cancel the loop later
+    let refreshInterval = setInterval((countdown)=>{
+        takePicture();
+        //update ui
+        //update countdown
+        //set 3 second timer?
+    },4000);//set to 4 seconds between pictures
+    //start countdown timer
+    //take pictures
+    //save to an array
+    //return array
+    for(let i = 0; i <3; i++){
+        
+    }
+    //stops the interval
+    clearInterval(refreshInterval);
 };
+
+function countdownTimer() {
+    let timer = document.querySelector("#countdown");
+    let countdownTime = 3;
+    setInterval(function(){
+        timer.innerHTML = countdownTime;
+        countdownTime--;
+        
+    },500);
+}
+
+function takePicture(){
+    countdownTimer();
+    context.drawImage(video,0,0,640,480);
+}
 
 function convertToImage(){
     let img = new Image();
