acf4036ac67de7caee0d5a7feb6fb35902df44c8
Tim Harshman <goteamtim+git@gmail.com>
2016-07-07 22:23:10 -0700

    Picture check

--- js/app.js
+++ js/app.js
@@ -24,6 +24,7 @@
 
     document.getElementById("takePicture").addEventListener("click", () => {
             takePictures();
+            takingPictures = true;
     });
 
 function buildPhotoStrip(imgArray){
